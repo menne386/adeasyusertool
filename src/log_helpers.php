34b9c7c5d4214e1a55c8d5d7424d84ba87b0864b
Menne Kamminga <mkamminga@bevolkingsonderzoeknoord.nl>
2019-04-11 10:12:16 +0200

    Security fix, dont want to be able to load random files from filesystem.

--- src/log_helpers.php
+++ src/log_helpers.php
@@ -18,7 +18,7 @@ function getLog($myDate) {
 	global $config;
 	global $ad_users;
 	global $ad_groups;
-	$file = $config['log_dir'].$myDate.'.log';
+	$file = $config['log_dir'].(int)$myDate.'.log';
 
 	$A = json_decode('{"entries":['.rtrim(rtrim(file_get_contents($file)),',').']}',true);	
 	$A['entries'] = array_reverse($A['entries']);
