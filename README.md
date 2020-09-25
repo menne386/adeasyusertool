c85fc91fb618dac9156b60d4c4ce0a2ecfc374b1
Menne Kamminga <kamminga.m@gmail.com>
2020-09-25 14:08:17 +0200

    Added LICENSE headers

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Easily create and manage users from a web interface.
 Tailored to allow non-technical users to add company users.
+This project was created to learn more about OATH TOTP protocol
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+// Copyright 2018 Menne Kamminga <kamminga DOT m AT gmail DOT com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 function checkPassword(e) {
 	if(e.target.type=='password') {
